097022d7e9d238d9cf2c290ea11232d6e2b02ed4
Tainara Alves <tadnascimento14@gmail.com>
2024-08-28 19:57:44 -0300

    Update aula 4

---
+++ Calculasalarios/Program.cs
@@ -0,0 +1,31 @@
+﻿string[] historico = new string[10];
+int indiceHistórico  = 0;
+
+while(true){
+    Console.Clear();
+    Console.WriteLine("\n\n\n\t\t\t\t\tMENU");
+    Console.WriteLine("Pressione F1 para calcular o salário líquido");
+    Console.WriteLine("Pressione F2 para calcular o 13° salário");
+    Console.WriteLine("Pressione F3 para exibir o histórico de cálculos");
+    Console.WriteLine("Pressione DEL para sair");
+    ConsoleKeyInfo tecla = Console.ReadKey(true);
+
+    if (tecla.Key == ConsoleKey.Enter)
+    {
+        Console.WriteLine("Digite o valor da hora trabalhada");
+        double valorHora = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o número de horas trabalhadas: ");
+        double horasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Digite o desconto total (em %):");
+        double desconto = Convert.ToDouble(Console.ReadLine());
+
+        double salarioBruto = valorHora * horasTrabalhadas;
+        double salarioLiquido = salarioBruto * (1 - desconto / 100);
+
+        Console.WriteLine($"O saláro bruto é: R${salarioBruto:F2}");
+        Console.WriteLine($"O saláro bruto é: R${salarioLiquido:F2}");
+
+    } // esta chave fecha o if do F1
+}
